b25f602824d7f747e0031c1442772002ce92bc70
Sudirman <sudirman.xtkj2@gmail.com>
2023-12-21 13:47:05 +0700

    update code controllers

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -13,18 +13,22 @@ exports.loginUser = async (req,res) =>{
       if(err) throw err;
       if(result.length === 0){
         return res.status(400).json({
+          status :"Gagal",
           msg:"Email belum terdaftar "});
       }
       const cekPassword = bcrypt.compareSync(password,result[0].password);
       if(!cekPassword){
         return res.status(400).json({
+          status: "Gagal",
           msg:"Password yang Anda masukan salah"});
       }
       const token = jwt.sign({
         id: result[0].id, 
+        username : result[0].username,
         email: result[0].email},'secret', { expiresIn: '1d' });
 
       return res.status(200).json({
+        status:"Sukses",
         msg:"Login berhasil",
         token
         
@@ -34,6 +38,7 @@ exports.loginUser = async (req,res) =>{
 
 exports.profile = async (req, res) => {
     return res.status(200).json({
+      status:"Sukses",
       user:req.user
     });
   }
@@ -44,13 +49,17 @@ exports.editUser = async (req,res) =>{
     const {username,email} = req.body;
   if(!username||!email){
       return res.status(400).json({
+        status:"Gagal",
         msg:"Username atau email yang Anda masukan tidak ditemukan"
       });
   }
   const query = "UPDATE db_user SET username=? WHERE email=?";
   db.query(query,[username,email],(err,result) =>{
       if(err) throw err;
-      return res.status(201).json({msg:"Username berhasil di-update"});
+      return res.status(201).json({
+        status : "Success",
+        msg:"Username berhasil di-update",
+      });
   })
   } catch (error) {
     console.log(error.massages);
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -7,12 +7,14 @@ exports.registerUser = async (req,res) =>{
     const {email,username,password,confirmasiPassword} = req.body;
     if(!email||!username||!password||!confirmasiPassword){
         return res.status(400).json({
+          status:"Gagal",
           msg:"Harap isi form yang diperlukan"
         });
     }
 
     if(confirmasiPassword !== password){
         return res.status(400).json({
+          status:"Gagal",
             message:"Password yang Anda masukan tidak cocok"
         })
     }
@@ -22,13 +24,16 @@ exports.registerUser = async (req,res) =>{
         if(err) throw err;
         if(result.length> 0){
             return res.status(400).json({
+              status:"Gagal",
               msg:"Email yang Anda masukan sudah pernah didaftarkan"});
         }
   
         const query = "INSERT INTO db_user(email,username,password) VALUES (?,?,?)";
         db.query(query,[email,username,bcrypt.hashSync(password,saltRounds)],(err,result) =>{
             if(err) throw err;
-            return res.status(201).json({msg:"Akun berhasil didaftarkan"});
+            return res.status(201).json({
+              status:"Sukses",
+              msg:"Akun berhasil didaftarkan"});
         })
     })  
     
